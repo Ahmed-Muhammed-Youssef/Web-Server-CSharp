a805c31eacccb38d49722c79b7d1b84855a7e6ab
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-08-20 13:54:04 +0300

    feat: add initial server component

---
+++ src/WebServer.Components/Server.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace WebServer.Components
+{
+    /// <summary>
+    /// Basic Web Server
+    /// </summary>
+    public class Server
+    {
+        private HttpListener _listener = new();
+
+        public Server(HttpListener? listener = null)
+        {
+            _listener = listener ?? new HttpListener();
+            InitializeListener();
+        }
+
+        public List<string> GetServerAddresses()
+        {
+            return [.. _listener.Prefixes];
+        }
+
+        /// <summary>
+        /// Returns list of IP addresses for every network interface for the current machine.
+        /// It doesn't include the loopback IP.
+        /// </summary>
+        private static List<IPAddress> GetLocalHostIPs()
+        {
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+
+            return host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToList();
+        }
+
+        /// <summary>
+        /// Initializes the listener to listen to all the network different interfaces
+        /// </summary>
+        private void InitializeListener()
+        {
+            var localhostIPs = GetLocalHostIPs() ?? [];
+
+            // The local host ips doesn't include the loopback ip
+            _listener.Prefixes.Add("http://localhost/");
+
+            localhostIPs.ForEach(ip =>
+            {
+                // Console.WriteLine("Listening on IP " + "http://" + ip.ToString() + "/");
+                _listener.Prefixes.Add("http://" + ip.ToString() + "/");
+            });
+        }
+    }
+}
---
+++ src/WebServer/Program.cs
@@ -0,0 +1,19 @@
+﻿using WebServer.Components;
+
+namespace WebServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new();
+
+            var ips = server.GetServerAddresses();
+
+            foreach (var ip in ips) {
+                Console.WriteLine("Listening on IP " + ip);
+            }
+
+        }
+    }
+}
