47a764ac5d2f189d9a65b781dd741d6d2536dc3c
Ahmed Youssef <ahmed.muhammed.youssef@gmail.com>
2024-08-20 17:02:11 +0300

    feat: add basic logger

--- src/WebServer.Components/Server.cs
+++ src/WebServer.Components/Server.cs
@@ -10,14 +10,16 @@ namespace WebServer.Components
     public class Server
     {
         private HttpListener _listener = new();
+        private readonly TextWriter _loggerDestination;
         private Semaphore sem;
 
-        public Server(int portNumber = 5000, int maxSimultaneousConnections = 20, HttpListener? listener = null)
+        public Server(int portNumber = 5000, int maxSimultaneousConnections = 20, HttpListener? listener = null, TextWriter? loggerDestination = null)
         {
             _listener = listener ?? new HttpListener();
+            this._loggerDestination = loggerDestination ?? Console.Out;
+            sem = new(maxSimultaneousConnections, maxSimultaneousConnections);
 
             InitializeListener(portNumber);
-            sem = new(maxSimultaneousConnections, maxSimultaneousConnections);
         }
 
         /// <summary>
@@ -52,6 +54,8 @@ namespace WebServer.Components
 
             // Release the semaphore so that another listener can be immediately started up.
             sem.Release();
+            
+            LogRequest(context.Request);
 
             SendResponse(context);
         }
@@ -77,6 +81,58 @@ namespace WebServer.Components
         private void InitializeListener(int portNumber)
         {
             _listener.Prefixes.Add("http://localhost:" + portNumber.ToString() + "/");
+        }
+
+        /// <summary>
+        /// Log requests.
+        /// </summary>
+        public void LogRequest(HttpListenerRequest request)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("HTTP Request Log:");
+            sb.AppendLine($"Timestamp: {DateTime.Now}");
+            sb.AppendLine($"Method: {request.HttpMethod}");
+            sb.AppendLine($"URL: {request.Url}");
+            sb.AppendLine($"Client IP: {request.RemoteEndPoint?.Address}");
+
+            // Log query string parameters
+            if (request.QueryString.Count > 0)
+            {
+                sb.AppendLine("Query String Parameters:");
+                foreach (string key in request.QueryString.AllKeys)
+                {
+                    sb.AppendLine($"    {key}: {request.QueryString[key]}");
+                }
+            }
+
+            // Log headers
+            sb.AppendLine("Headers:");
+            foreach (string key in request.Headers.AllKeys)
+            {
+                sb.AppendLine($"    {key}: {request.Headers[key]}");
+            }
+
+            // Optionally log request body if it's not a GET request and has content
+            if (request.HttpMethod != "GET" && request.HasEntityBody)
+            {
+                sb.AppendLine("Body:");
+                using (var bodyReader = new StreamReader(request.InputStream, request.ContentEncoding))
+                {
+                    sb.AppendLine(bodyReader.ReadToEnd());
+                }
+            }
+
+            sb.AppendLine("End of Request Log");
+            sb.AppendLine(new string('-', 50));
+
+            // Write the log to the configured writer
+            Log(sb.ToString());
+        }
+
+        private void Log(string info)
+        {
+            _loggerDestination.WriteLine(info);
         }
     }
 }
